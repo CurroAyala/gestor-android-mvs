196c47c17e0f8719f178a87c697c42d9a22abc91
CurroAyala <curroayala18@gmail.com>
2026-02-27 14:33:14 +0100

    chore: dependencia de Room añadida

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,4 +41,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    implementation(libs.room.runtime)
+    implementation(libs.room.ktx)
+    annotationProcessor(libs.room.compiler)
 }
