6600feebd47ade78c87c740cd3627217b2b48628
CurroAyala <curroayala18@gmail.com>
2026-03-07 18:48:47 +0100

    feat: MainActivity añadida

---
+++ app/src/main/java/com/curro/gestormvs/ui/MainActivity.java
@@ -0,0 +1,61 @@
+package com.curro.gestormvs.ui;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.curro.gestormvs.R;
+import com.curro.gestormvs.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private NavController navController;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        setSupportActionBar(binding.topAppBar);
+
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.nav_host_fragment);
+
+        if (navHostFragment != null) {
+            navController = navHostFragment.getNavController();
+
+            AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                    //TODO: añadir los IDs del nav_graph.xml de las pantallas principales
+                    // (Inicio, añadir host y ajustes)
+            ).build();
+
+            NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+            NavigationUI.setupWithNavController(binding.bottomNavigation, navController);
+        }
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return (navController != null && navController.navigateUp()) || super.onSupportNavigateUp();
+    }
+
+}
