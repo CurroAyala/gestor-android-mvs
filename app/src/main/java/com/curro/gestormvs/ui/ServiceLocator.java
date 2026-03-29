b425fea9c81d214954bc3310209b2a59b0773d96
CurroAyala <curroayala18@gmail.com>
2026-03-29 22:15:21 +0200

    feat: Método para proporcionar el repositorio de SSH añadido

--- app/src/main/java/com/curro/gestormvs/ui/ServiceLocator.java
+++ app/src/main/java/com/curro/gestormvs/ui/ServiceLocator.java
@@ -5,10 +5,12 @@ import android.content.Context;
 import com.curro.gestormvs.data.daos.HostDao;
 import com.curro.gestormvs.data.db.AppDatabase;
 import com.curro.gestormvs.data.repositories.HostRepository;
+import com.curro.gestormvs.data.repositories.SshRepository;
 
 public class ServiceLocator {
 
     private static HostRepository hostRepository;
+    private static SshRepository sshRepository;
 
     public static HostRepository provideHostRepository(Context context) {
         if (hostRepository == null) {
@@ -16,6 +18,13 @@ public class ServiceLocator {
             hostRepository = new HostRepository(dao, context);
         }
         return hostRepository;
+    }
+
+    public static SshRepository provideSshRepository() {
+        if (sshRepository == null) {
+            sshRepository = new SshRepository();
+        }
+        return sshRepository;
     }
 
 }
