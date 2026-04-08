f865c76290136bcc71823a11819a98e1a0c1f77b
CurroAyala <curroayala18@gmail.com>
2026-04-08 16:35:34 +0200

    feat: Caso de uso para reiniciar MVs

---
+++ app/src/main/java/com/curro/gestormvs/domain/useCases/RebootVMUseCase.java
@@ -0,0 +1,52 @@
+package com.curro.gestormvs.domain.useCases;
+
+import com.curro.gestormvs.data.repositories.SshRepository;
+import com.curro.gestormvs.domain.models.VirtualMachine;
+
+public class RebootVMUseCase {
+
+    private final SshRepository sshRepository;
+    private final CheckVMStateUseCase checkVMStateUseCase;
+
+
+    public RebootVMUseCase(SshRepository sshRepository, CheckVMStateUseCase checkVMStateUseCase) {
+        this.sshRepository = sshRepository;
+        this.checkVMStateUseCase = checkVMStateUseCase;
+    }
+
+
+    // Use case -------------------------
+    public void execute(VirtualMachine vm) {
+        if (vm == null) throw new IllegalArgumentException("Virtual Machine cannot be null");
+        if (!vm.getState().equalsIgnoreCase("running")) {
+            throw new IllegalArgumentException("Only running virtual machines can be restarted");
+        }
+
+        // Using virsh command "reboot" is avoided because it is a soft reboot, what means that
+        // VMs keep their state as "running", preventing state checking.
+
+        sshRepository.shutdownVM(vm);
+
+        boolean isDown = checkVMStateUseCase.execute(vm.getName(), "shut off");
+        if (!isDown) {
+            throw new RuntimeException("Error shutting down. " +
+                    "It took too long.");
+        }
+
+        sshRepository.startVM(vm);
+
+        boolean isUp = checkVMStateUseCase.execute(vm.getName(), "running");
+        if (!isUp) {
+            throw new RuntimeException("Error starting. " +
+                    "It took too long.");
+        }
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+}
